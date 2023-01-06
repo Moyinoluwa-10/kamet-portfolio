4a6b22463070c7d40316c40d2547f201a0a2042a
Moyinoluwa-10 <moyinadelowo@gmail.com>
2023-01-06 06:17:25 +0100

    chore: fixed navlink issue

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ hamburger.addEventListener("click", () => {
   navMenu.classList.toggle("active");
 });
 
-document.querySelectorAll("nav-link").forEach((n) =>
+document.querySelectorAll(".nav_link").forEach((n) =>
   n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
